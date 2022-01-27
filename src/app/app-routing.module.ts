31dc3b86ffb2bf97bc77743d8c3909b9763b5201
root <frutmeteam@gmail.com>
2022-01-27 13:34:17 +0000

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+{ path: 'home', loadChildren: () => import('./components/pages/home/home.module').then(m => m.HomeModule) },
+{ path: 'login', loadChildren: () => import('./components/pages/login/login.module').then(m => m.LoginModule) }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
