507bf791c6363ccaad8b427879d6f39adb127efc
root <frutmeteam@gmail.com>
2022-02-07 05:32:40 +0000

    bikers

--- src/app/components/pages/login/login.component.ts
+++ src/app/components/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Butler } from '@app/services/butler.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public _butler:Butler
+  ) { }
+
 
   ngOnInit(): void {
+  this._butler.biker=false;
   }
 
 }
