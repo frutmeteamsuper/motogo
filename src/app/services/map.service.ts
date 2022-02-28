ea8bc301d11bc102bd3986ea7ae06b1a9c6fb3f5
root <frutmeteam@gmail.com>
2022-02-28 04:24:36 +0000

    places

--- src/app/services/map.service.ts
+++ src/app/services/map.service.ts
@@ -1,13 +1,36 @@
 import { Injectable } from '@angular/core'; 
-import {LngLatLike, Map} from'mapbox-gl';
+import {LngLatLike, Map,Marker, Popup} from'mapbox-gl';
+import { Feature } from '@app/interfaces/places';
+
 @Injectable({
   providedIn: 'root'
 })
 export class MapService {
   private map?:Map;
+  private markers:Marker[]=[]
+
 
   get isMapReady(){
     return !!this.map;
+  }
+  createMarkersFromPlaces(places:Feature[]){
+    if(!this.map)throw Error('mapnot');
+    this.markers!.forEach(marker=>marker.remove());
+    const newMarkers=[];
+    for (const place of places){
+      const [lng,lat  ]=place.center;
+      const popup =new Popup()
+      .setHTML(`
+      <h6>${place.text}</h6>
+      </span>${place.place_name}</span>
+        `);
+        const newMarker = new Marker()
+        .setLngLat([lng,lat])  
+        .setPopup(popup)
+        .addTo(this.map);
+        newMarkers.push(newMarker);  
+    }
+    this.markers=newMarkers;
   }
   setMap (map:Map){
     this.map=map;
